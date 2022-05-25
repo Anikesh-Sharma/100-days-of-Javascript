bbd47540f82f56f1f7f821ec287b5433e974a311
Anikesh Sharma <91007286+Anikesh-Sharma@users.noreply.github.com>
2022-05-25 17:35:03 +0530

    Add files via upload
    Hi Developer,
    
    I am Anikesh Sharma and I will post top 100 projects  of JavaScript for beginners.
    Journey begins from today.
    I will build 1 projects per day and post it there
    Hope you like it 
    
    Thank you for your support

---
+++ script.js
@@ -0,0 +1,17 @@
+const number = document.querySelector(".number");
+const btn = document.querySelector(".generate");
+
+const generateNumber = () => {
+  // Generate number btn 1 and 10
+  const randomNumber = Math.floor(Math.random() * 10 + 1);
+  number.innerHTML = randomNumber;
+  console.log(randomNumber);
+};
+
+btn.addEventListener("click", generateNumber);
+
+generateNumber();
+
+
+// const rand = Math.floor(Math.random() * 10 + 1);
+// console.log(rand);
